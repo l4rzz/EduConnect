199993b111dcb701728841c3bd6f61c56f02baa9
Rutger van de Klundert <rutger_vdk@hotmail.com>
2019-12-13 09:41:04 +0100

    teacher working

--- backend/target/Teamnull/WEB-INF/template/scripts/signup.js
+++ backend/target/Teamnull/WEB-INF/template/scripts/signup.js
@@ -2,6 +2,8 @@ $( document ).ready(function() {
     
     $('#submit-teacher').click( function(event){
 
+        var usernameTeacher = $("#username-teacher").val();
+        var passwordTeacher = $("#password-teacher").val();
         var emailTeacher = $("#email-teacher").val();
         var nameTeacher = $("#name-teacher").val();
         var imageLinkTeacher = $("#imageLink-teacher").val();
@@ -20,25 +22,34 @@ $( document ).ready(function() {
             contentType: 'application/json'
         })
     });
+});
+    
 
-    $('#submit-school').click( function(event){
-
-        var emailSchool = $("#email-school").val();
-        var nameSchool = $("#name-school").val();
-        var imageLinkSchool = $("#imageLink-school").val();
+    /*
+    $("#signup-school").click(function() {
+        var usernameSchool = $("#username-school").val();
+        var passwordSchool = $("#password-school").val();
+        var emailadressSchool = $("#email-school").val();
+        var fullnameSchool = $("#name-school").val();
         var subjectSchool = $("#subject-school").val();
 
         $.ajax({
-            url: 'http://localhost:8080/teamnull/api/school/add',
+            url: 'http://localhost:8080/teamnull/api/school',
             type: 'POST',
             data: JSON.stringify({
-                email: emailSchool,
-                imageLink: imageLinkSchool,
-                name: nameSchool,
+                username: usernameSchool,
+                password: passwordSchool,
+                email: emailadressSchool,
+                name: fullnameSchool,
                 subject: subjectSchool
             }),
             async: true,
-            contentType: 'application/json'
-        })
+            contentType: 'application/json',
+            success: successCallback,
+            error: errorCallback,
+
+        });
     });
+   
 });
+ */
